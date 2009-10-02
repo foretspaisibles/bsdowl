69f4301fd575fd2e392fb2591dd86a46a558e8c6
Michael Grünewald <michipili@gmail.com>
2009-10-02 20:06:47 +0000

    Adoption de la license CeCILL-B
    Un programme a été utilisé pour modifier les en-têtes des fichiers
    contenant des informations sur la license. Ce programme est
    enregistré dans le projet sous
    
      Library/Ancillary/modify_license.sh
    
    Ce programme n'est pas solide du tout, mais peut-être réutilisé après
    modifications adéquates en d'autres occasions.
    --Cette ligne, et les suivantes ci-dessous, seront ignorées--
    
    M    test/ocaml/test.ocaml.prog.mk
    M    test/ocaml/yoyodyne.mk
    M    bps/bps.project.mk
    M    bps/bps.subdir.mk
    M    bps/bps.init.mk.in
    M    bps/bps.objdir.mk
    M    bps/bps.credentials.mk
    M    bps/bps.autoconf.mk
    M    bps/bps.clean.mk
    M    bps/bps.files.mk
    M    bps/bps.own.mk
    M    bps/bps.usertarget.mk
    M    text/latex.bibtex.mk
    M    text/tex.driver.dvi.mk
    M    text/latex.files.mk
    M    text/tex.driver.dvips.mk
    M    text/tex.device.dvi.mk
    M    text/tex.device.ps.mk
    M    text/latex.doc.mk
    M    text/tex.files.mk
    M    text/tex.mpost.mk
    M    text/tex.init.mk
    M    text/tex.doc.mk
    M    text/tex.doc.main.mk
    M    www/www.files.mk
    M    ocaml/ocaml.yacc.mk
    M    ocaml/ocaml.find.mk
    M    ocaml/ocaml.odoc.mk
    M    ocaml/ocaml.main.mk
    M    ocaml/ocaml.target.mk
    M    ocaml/ocaml.lex.mk
    M    ocaml/ocaml.flags.mk
    M    ocaml/ocaml.searches.mk
    M    ocaml/ocaml.depend.mk
    M    ocaml/ocaml.build.mk
    M    ocaml/ocaml.source.mk
    M    ocaml/ocaml.tools.mk
    M    ocaml/ocaml.init.mk
    M    ocaml/ocaml.object.mk
    M    ocaml/ocaml.lib.mk
    M    ocaml/ocaml.prog.mk
    M    ocaml/ocaml.template.mk
    M    ocaml/ocaml.manual.mk
    M    ocaml/ocaml.pack.mk
    M    misc/misc.script.mk
    M    misc/misc.dotfile.mk
    M    misc/misc.elisp.mk
    M    Library/Template/makefile.mk
    M    Library/Make/pallas.mk
    M    Library/Make/script.mk
    M    Library/Make/project.mk
    M    Library/Make/subdir.mk
    M    Library/Ancillary/insert_licence.sh
    A    Library/Ancillary/modify_license.sh
    M    Library/Ancillary/mkheader.sh
    M    Library/Ancillary/trampoline
    M    Library/Ancillary/environ.csh
    M    Library/Ancillary/releng
    M    Library/Ancillary/insert_licence.ed
    M    snippets/for_match.mk
    M    rox/rox.init.mk
    M    rox/rox.mime.mk
    M    rox/rox.template.mk
    M    rox/rox.sendto.mk
    M    COPYING
    A    COPYING-FR
    
    
    git-svn-id: svn+ssh://svn.gna.org/svn/bsdmakepscripts/trunk@164 3103e521-a7b5-4707-98a8-f8ec597c7033

--- Library/Ancillary/mkheader.sh
+++ Library/Ancillary/mkheader.sh
@@ -6,36 +6,16 @@
 
 # $Id$
 
-# Copyright (c) 2006, 2007, 2008, 2009 Michaël Le Barbier Grünewald
-# All rights reserved.
+# BSDMake Pallàs Scripts (http://home.gna.org/bsdmakepscripts/)
+# This file is part of BSDMake Pallàs Scripts
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
+# Copyright (C) Michaël Le Barbier Grünewald - 2006-2009
 #
-#    1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#
-#    2. Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#
-#    3. The name of the author may not be used to endorse or promote
-#    products derived from this software without specific prior written
-#    permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
-# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
-# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
+# This file must be used under the terms of the CeCILL-B.
+# This source file is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt
 
 mkheader_process()
 {
