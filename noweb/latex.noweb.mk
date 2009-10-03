2ef1da5cf1f260845f7df4ac6d136cfa965e003e
Michael Grünewald <michipili@gmail.com>
2009-10-03 20:32:23 +0000

    Support pour NOWEB
    git-svn-id: svn+ssh://svn.gna.org/svn/bsdmakepscripts/trunk@172 3103e521-a7b5-4707-98a8-f8ec597c7033

---
+++ noweb/latex.noweb.mk
@@ -0,0 +1,38 @@
+### latex.noweb.mk -- Préparation de macros LaTeX avec NOWEB
+
+# Author: Michaël Le Barbier Grünewald
+# Date: Sam  3 oct 2009 19:10:53 CEST
+# Lang: fr_FR.ISO8859-15
+
+# $Id$
+
+# BSDMake Pallàs Scripts (http://home.gna.org/bsdmakepscripts/)
+# This file is part of BSDMake Pallàs Scripts
+# 
+# Copyright (C) Michaël Le Barbier Grünewald - 2009
+# 
+# This file must be used under the terms of the CeCILL-B.
+# This source file is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt
+
+NOWEAVE_AUTODEFS = tex
+NOWEAVE_LATEX_WRAPPER = delay
+
+NOWEAVE_LATEX_DEFS?= nwlatex.tex
+NOWEAVE_HTML_CSS?= nwlatex.css
+
+_TEX_DOC = ${NOWEAVE}
+_TEX_DOC+= sampleart
+
+SRCS.sampleart = ${NOTANGLE} sharticle.cls sampleart.tex
+
+FORMAT = latex
+DOCUMENT+= ${NOWEAVE:=.html}
+TEXSRC+= ${NOTANGLE}
+
+.include "bps.project.mk"
+.include "main.noweb.mk"
+.include "tex.files.mk"
+.include "latex.doc.mk"
