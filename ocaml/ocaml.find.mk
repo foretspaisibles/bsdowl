cf516fa7de772ef29c6aa019a15ad5acce278fb2
Michael Grünewald <michipili@gmail.com>
2013-07-03 05:54:30 +0000

    OCaml toplevel
    git-svn-id: svn+ssh://svn.gna.org/svn/bsdmakepscripts/trunk@347 3103e521-a7b5-4707-98a8-f8ec597c7033

--- ocaml/ocaml.find.mk
+++ ocaml/ocaml.find.mk
@@ -28,23 +28,30 @@
 
 ### DESCRIPTION
 
-# Ce module détermine grâce à la présence d'une liaison pour l'une des
-# variables PACKAGES, PREDICATES ou USE_OCAMLFIND, si l'usager
-# souhaite utiliser ocamlfind. Dans ce cas, ce module les
-# pseudo-commandes pour la compilation et l'édition de lien avec
-# OCAMLFIND.
+# We provide virtual tools based on the ocamnlfind utility upon
+# explicit request of the user (USE_OCAMLFIND) or when idiosyncratic
+# variables are defined (PACKAGES, PREDICATES).
 
 
 .if !target(__<ocaml.find.mk>__)
 __<ocaml.find.mk>__:
 
+#
+# Detect idiosyncratic variables
+#
+
 .if defined(PACKAGES)&&!empty(PACKAGES)
-USE_OCAMLFIND=yes
+USE_OCAMLFIND?=yes
 .endif
 
 .if defined(PREDICATES)&&!empty(PREDICATES)
-USE_OCAMLFIND=yes
+USE_OCAMLFIND?=yes
 .endif
+
+
+#
+# Conditionally define virtual tools
+#
 
 USE_OCAMLFIND?=no
 
@@ -52,6 +59,7 @@ USE_OCAMLFIND?=no
 MLCB?= ocamlfind ocamlc -c
 MLCN?= ocamlfind ocamlopt -c
 OCAMLDOC?= ocamlfind ocamldoc
+OCAMLMKTOP?= ocamlfind ocamlmktop
 .if !empty(TARGET:Mbyte_code)
 MLCI?= ocamlfind ocamlc -c
 .else
@@ -61,7 +69,7 @@ MLLB?= ocamlfind ocamlc -linkpkg
 MLLN?= ocamlfind ocamlopt -linkpkg
 .endif
 
-.for pseudo in MLCB MLCN MLCI MLLB MLLN OCAMLDOC
+.for pseudo in MLCB MLCN MLCI MLLB MLLN OCAMLDOC OCAMLMKTOP
 .if defined(PACKAGES)&&!empty(PACKAGES)
 ${pseudo}+= -package "${PACKAGES}"
 .endif
---
+++ ocaml/ocaml.toplevel.mk
@@ -0,0 +1,160 @@
+### ocaml.odoc.mk -- Interface simple avec OCamldoc
+
+# Author: Michael Grünewald
+# Date: dim 16 jui 2013 13:54:55 CEST
+
+# BSDMake Pallàs Scripts (http://home.gna.org/bsdmakepscripts/)
+# This file is part of BSDMake Pallàs Scripts
+#
+# Copyright (C) 2013 Michael Grünewald
+#
+# This file must be used under the terms of the CeCILL-B.
+# This source file is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt
+
+
+### SYNOPSIS
+
+# This simple interface with ocamlmktop allows you produce toplevels.
+
+# TOPLEVEL = toplevel
+# SRCS = initialize_toplevel.ml
+# LIBS = unix
+# LIBS+= str
+#
+# .include "ocaml.odoc.mk"
+
+
+### DESCRIPTION
+
+# Targets:
+#
+# do-build-toplevel
+# do-install-toplevel
+# do-clean-toplevel
+
+# Variables:
+#
+#
+# TOPLEVEL
+#   UNIX file name of the generated toplevel
+#
+#
+# SEARCHES
+#   Lookup path for object files
+#
+#   Relative paths are interpreted from ${.OBJDIR}.
+#
+#
+# SRCS
+#   OCaml implementation files
+#
+#
+# LIBS
+#   OCaml library files
+#
+#
+# PACKAGES
+#   OCaml findlib packages
+#
+#
+# TOPLEVEL_CUSTOM
+#   Flag governing generation of a custom toplevel
+#
+#
+# TOPLEVEL_COPT
+#   Pass the given options to the C compiler and linker
+#
+#   This forces TOPLEVEL_CUSTOM to yes.
+#
+#
+# TOPLEVEL_CLIB
+#   Pass the given library names to the C linker
+#
+#   This forces TOPLEVEL_CUSTOM to yes.
+
+
+### RÉALISATION
+
+.if !target(__<ocaml.toplevel.mk>__)
+__<ocaml.toplevel.mk>__:
+
+.include "bps.init.mk"
+.include "ocaml.init.mk"
+
+
+.if !defined(TOPLEVEL)||empty(TOPLEVEL)
+.error The ocaml.toplevel.mk module expects you to set the TOPLEVEL variable to a sensible value.
+.endif
+
+TOPLEVEL_CUSTOM?= no
+TOPLEVEL_COPT?=
+TOPLEVEL_CLIB?=
+
+OCAMLMKTOP?= ocamlmktop
+
+_TOPLEVEL_FLAGS=-custom -linkall -dllpath-all -linkpkg
+
+#
+# Determining toplevel flags
+#
+
+.if defined(TOPLEVEL_COPT)&&!empty(TOPLEVEL_COPT)
+TOPLEVEL_CUSTOM=yes
+.endif
+
+.if defined(TOPLEVEL_CLIB)&&!empty(TOPLEVEL_CLIB)
+TOPLEVEL_CUSTOM=yes
+.endif
+
+.if ${TOPLEVEL_CUSTOM} == yes
+_TOPLEVEL_FLAGS+=-custom
+.endif
+
+.if defined(TOPLEVEL_COPT)&&!empty(TOPLEVEL_COPT)
+.for item in ${TOPLEVEL_COPT}
+_TOPLEVEL_FLAGS+=-ccopt ${item}
+.endfor
+.endif
+
+.if defined(TOPLEVEL_CLIB)&&!empty(TOPLEVEL_CLIB)
+.for item in ${TOPLEVEL_CLIB}
+_TOPLEVEL_FLAGS+=-cclib -l${item}
+.endfor
+.endif
+
+.if defined(PACKAGES)&&!empty(PACKAGES)
+_TOPLEVEL_FLAGS+= -package "${PACKAGES}"
+.endif
+
+.if defined(SEARCHES)&&!empty(SEARCHES)
+.for item in ${SEARCHES}
+_TOPLEVEL_FLAGS+=-I ${item}
+.endfor
+.endif
+
+.for file in ${SRCS}
+_OCAML_CMO+= ${file:.ml=.cmo}
+${TOPLEVEL}: ${file:.ml=.cmo}
+.endfor
+
+.for file in ${LIBS}
+${TOPLEVEL}: ${file:=.cma}
+.endfor
+
+${TOPLEVEL}:
+	${OCAMLMKTOP} ${_TOPLEVEL_FLAGS} ${.ALLSRC} -o ${.TARGET}
+
+CLEANFILES+=    ${TOPLEVEL}
+BIN+=           ${TOPLEVEL}
+
+.include "ocaml.main.mk"
+.include "bps.clean.mk"
+.include "bps.files.mk"
+.include "bps.usertarget.mk"
+
+.endif # !target(__<ocaml.toplevel.mk>__)
+
+### End of file `ocaml.odoc.mk'
