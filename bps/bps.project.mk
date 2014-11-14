203d5594d02fe3b0fc2f6b67e5b94a3e5bd6baf4
Michael Grünewald <michipili@gmail.com>
2014-11-14 16:35:52 +0100

    Rewrite the project module

--- bps/bps.project.mk
+++ bps/bps.project.mk
@@ -1,4 +1,4 @@
-### bps.project.mk -- Maintenance pour de petits projets
+### bps.project.mk -- Maintenance for software projects
 
 # Author: Michael Grünewald
 # Date: Sam 19 avr 2008 16:27:56 CEST
@@ -17,244 +17,130 @@
 
 ### SYNOPSIS
 
-# PROJECTVERSION =	1.1
-# PROJECTNAME =		projectpublication
-# PROJECTAUTHOR =	The name of the GPG guy
-# PROJECTDISTDIR =	/attic
+# PACKAGE=		heat
+# VERSION =		1.1.0
+# OFFICER=		release-engineer@heat.org
+#
+# MODULE=		langc.lib:librational
+# MODULE+=		langc.lib:libfibonacci
+# MODULE+=		langc.prog:heat
 #
 # .include "bps.project.mk"
 
 
 ### DESCRIPTION
 
-# Le support de maintenance pour de petits projets fournit une
-# assistance pour les opérations suivantes:
-#  -- la préparation d'archives `tar';
-#  -- la publication de ces archives (avec signature).
+# This maintenance module for software projects should be the “master”
+# Makefile of a package source tree.
 #
-# La publication des archives et des signatures se fait vers un point du
-# système de fichiers, la publication vers un serveur ouvert au public
-# nécessite en général des manipulations supplémentaires.
+# Beyond supporting the build and test of the software project, it
+# also supports other functions like linting or the production of
+# GPG-signed tarballs, and the preparation of a developper's subshell.
 
 
+# Variables:
 #
-# Description des variables
 #
-
-# PROJECTVERSION
+#  SRCDIR, WRKDIR, PACKAGE, PACKAGEDIR, VERSION, OFFICER, MODULE, EXTERNAL
+#   See bps.package.mk
 #
-#   Version du projet, par exemple: 1.1
 #
-#   Si cette variable n'est pas initialisée mais que VERSION l'est,
-#   cette dernière est utilisée pour initialiser PROJECTVERSION. En
-#   l'absence d'initialisation explicite, la valeur 0.0 est
-#   implicitement affectée à PROJECTVERSION.
-
-# PROJECTNAME
+#  DISTDIR [${.OBJDIR}]
+#   The folder used to publish distribution files and signatures
 #
-#   Nom du projet, par exemple: bsdmakepscript
+#   A useful value could be 'DISTDIR=${HOME}/Distfiles/${PACKAGE}/${VERSION}'.
+#   It can be set from the environment but special care has to be
+#   taken to prevent the expansion of ${PACKAGE} and ${VERSION} by
+#   the shell.
 #
-#   Si cette variable n'est pas initialisée mais que PACKAGE,
-#   PROJECT ou NAME l'est, cette variable est utilisée pour
-#   initialiser PROJECTNAME. En l'absence d'initialisation
-#   explicite, une valeur déduite du nom du répertoire racine du
-#   projet est implicitement affectée à PROJECTNAME.
-
-# PROJECTAUTHOR
 #
-#   Auteur du projet, par exemple: Michael Grünewald
+#  DISTNAME [${PACKAGENAME}-${VERSION}]
+#   The name used to produce tarballs
 #
-#   La valeur de cette variable est utilisée lors de l'étape de
-#   signature pour déterminer la clef à utiliser.
+#   This name is also the name of the root directory appearing at the
+#   root of the archive.
 #
-#   Si cette variable n'est pas initialisée mais que AUTHOR l'est,
-#   cette dernière est utilisée pour initialiser PROJECTAUTHOR. En
-#   l'absence d'initialisation explicite, la variable n'est pas
-#   initialisée et la valeur de USE_PROJECT_GPG est positionnée à no.
-
-# PROJECTDISTDIR
 #
-#   Dossier où sont placées les archives avant d'être publiées. Ce
-#   dossier reçoit aussi les fichiers de signature.
+#  DISTSIGN
+#   The list of extra distribution files which should be signed
 #
-#   Si cette variable n'est pas initialisée mais que DISTDIR l'est,
-#   cette dernière est utilisée pour initialiser PROJECTDISTDIR. En
-#   l'absence d'initialisation explicite, la valeur ${.OBJDIR}
-#   implicitement affectée à PROJECTDISTDIR.
-
-# PROJECTDISTNAME
 #
-#   Le nom des archives, qui est aussi le nom du dossier racine
-#   apparaissant dans l'archive.
+#  DISTNOSIGN
+#   The list of extra distribution files which do not need to be signed
 #
-#   La valeur implicite de PROJECTDISTNAME est
-#   ${PROJECTNAME}-${PROJECTVERSION}.
-
-# PROJECTDIST
 #
-#   Liste de fichiers supplémentaires devant aussi faire l'objet d'une
-#   publication non signée.
+#  DISTEXCLUDE [set by a strategy]
+#   The list of files or directories which should not be distributed
 #
-#   Les fichiers enumérés dans cette variable sont publiés aux côtés
-#   des archives et des signatures des archives.
-
-# PROJECTDISTSIGN
+#   The files enumerated by the DISTEXCLUDE variable are excluded from
+#   the tarball distribution. The list of files produced by autoconf
+#   is automatically added to this list.
 #
-#   Liste de fichiers supplémentaires devant aussi faire l'objet d'une
-#   publication signée.
 #
-#   Les fichiers enumérés dans cette variable sont publiés aux côtés
-#   des archives et des signatures des archives.
-
-# PROJECTDISTEXCLUDE
+#  PROJECTENV
+#   The list of variables passed to the environment of subshells
 #
-#   Liste de fichiers à ne pas inclure dans les archives publiées.
-#   Le module ajoute automatiquement les fichiers objets produits par
-#   le script configure à cette liste (voir bps.autoconf.mk).
-
-# USE_PROJECT_GPG
 #
-#   Contôle l'utilisation de GPG pour signer les fichiers publiés. Si
-#   cette variable est positionnée à une autre valeur que yes, les
-#   fichiers sont publiés sans être préalablement signés.
-
-# PROJECTENV
+#  PROJECTLIBRARY [Library]
+#   The folder used as project library
 #
-#   Énumère les variables d'environnement exportées vers les sous-shells
-
-# PROJECTBASE
 #
-#   Dossier principal du projet
+#  PROJECTLIBRARYMAKE [Library/Make]
+#   The folder holding project specific makefiles
 #
-#   Cette variable est initialisée automatiquement à partir de .CURDIR.
-
-# PROJECTLIBRARY
 #
-#   Bibliothèque du projet
+#  PROJECTLIBRARYSHELL [Library/Ancillary]
+#   The folder holding project specific scripts
 #
-#   Nom d'un dossier «bibliothèque» contenant des sous-dossiers pour
-#   les modèles utilisés dans le projet, les makefiles spécifiques,
-#   etc.
-
-# PROJECTLIBRARYMAKE
 #
-#   Bibliothèque de fichiers Make
+#  SUBSHELLDIR [not set]
+#   A folder to change to before popping up a subshell
 
 
+# Targets:
 #
-# Description des cibles
+#  Each target has the classical decomposition “pre-do-post” which
+#  eases their customisation.
 #
-
-# Toutes les cibles sont décomposées en étapes `pre-do-post' pour
-# pouvoir accueillir le plus hospitalièrement possible les
-# modifications de l'utilisateur.
-
-# dist:
 #
-#   Crée les archives destinées à la publication.
-#   Ces archives sont placées dans le dossier PROJECTDISTDIR.
+#  dist
+#   Create, sign and publish distribution files
 #
-#   Note importante: Le programme GNU tar ne dispose pas d'une option
-#   «déréférencer les liens présents sur la ligne de commande». À cause de
-#   cette restriction, il est interdit d'utiliser un lien symbolique dans
-#   l'arbre des sources d'un projet.
-
-# publish:
+#    Important note fot GNU tar users: The GNU tar command does not
+#    have an option “dereference symbolic links provided on the
+#    command line”.  It is therefore forbidden to use symbolic links in
+#    the source tree of a project.
 #
-#   Prépare les archives, les signe, et les publie.
-
-# prepublish:
 #
-#   Comme publish, mais s'arrête juste avant la publication proprement
-#   dite.
-
-# subshell:
-#
-#   Ouvre un sous-shell pour le développeur
-#
-#   L'environnement du shell ouvert par cette cible contient les
-#   liaisons énumérées dans PROJECTENV.
-#
-#   Si cette variable est définie, entre dans le dossier SUBSHELLDIR
-#   avant d'éxécuter ce nouveau shell.
-
-# update:
-#
-#   Permet la mise à jour automatique des sources du projet
-#
-# Cette cible n'est associée à aucune rectette, mais l'utilisateur
-# peut en assigner une, s'il souahite automatiser automatiquement les
-# soruces de son projet.
+# subshell
+#  Pop up a user subshell
 
 
-### IMPLÉMENTATION
+### IMPLEMENTATION
 
 .if !target(__<bps.project.mk>__)
 __<bps.project.mk>__:
 
-#
-# Initialistion des variables
-#
-.if !defined(PROJECTNAME)&&defined(PACKAGE)
-PROJECTNAME = ${PACKAGE}
-.endif
-.if !defined(PROJECTNAME)&&defined(PROJECT)
-PROJECTNAME = ${PROJECT}
-.endif
-.if !defined(PROJECTNAME)&&defined(NAME)
-PROJECTNAME = ${NAME}
-.endif
-.if !defined(PACKAGE)&&defined(PROJECTNAME)
-PACKAGE = ${PROJECTNAME}
-.endif
-# La définition possible de PACKAGEDIR ci-dessous est redondante
-# de celle figurant dans `bps.init.mk' mais il se peut que la variable
-# PACKAGEDIR ait tout-juste été définie.
-.if defined(PACKAGE)&&!empty(PACKAGE)
-PACKAGEDIR?= /${PACKAGE}
-.endif
-.if !defined(PROJECTAUTHOR)&&defined(AUTHOR)
-PROJECTAUTHOR = ${AUTHOR}
-.endif
-.if !defined(PROJECTVERSION)&&defined(VERSION)
-PROJECTVERSION = ${VERSION}
-.endif
-.if !defined(PROJECTDISTDIR)&&defined(DISTDIR)
-PROJECTDISTDIR = ${DISTDIR}
-.endif
-# Les variables permettant de deviner les valeurs pour le module
-# PROJECT ont toutes été positionnées, on passe à l'initialistion à
-# l'aide de valeurs implicites.
-.if !defined(PROJECTNAME)||empty(PROJECTNAME)
-PROJECTNAME = ${.CURDIR:T}
-.endif
-.if !defined(PROJECTVERSION)||empty(PROJECTVERSION)
-PROJECTVERSION = 0.0
-.endif
-.if !defined(PROJECTAUTHOR)||empty(PROJECTAUTHOR)
-USE_PROJECT_GPG = no
-.endif
-GPG?= gpg
-USE_PROJECT_GPG?= yes
-PROJECTDIST?=
-PROJECTDISTSIGN?=
-PROJECTDISTNAME?= ${PROJECTNAME}-${PROJECTVERSION}
-PROJECTDISTDIR?= ${.OBJDIR}
+DISTNAME?=		${PACKAGE}-${VERSION}
+DISTDIR?=		${.OBJDIR}
+GPG?=			${_BPS_GPG}
+
+.include "bps.init.mk"
 
 
 #
-# Structures pour le module de compression
+# Data for compression tools
 #
 
-_PROJECT_COMPRESS_TOOLS?=${_BPS_COMPRESS}
-_PROJECT_COMPRESS.suffix.none =
-_PROJECT_COMPRESS.suffix.gzip = .gz
-_PROJECT_COMPRESS.suffix.bzip2 = .bz2
-_PROJECT_COMPRESS.suffix.xz = .xz
-_PROJECT_COMPRESS.flag.none =
-_PROJECT_COMPRESS.flag.gzip = -z
-_PROJECT_COMPRESS.flag.bzip2 = -j
-_PROJECT_COMPRESS.flag.xz = -J
+_PROJECT_COMPRESS_TOOLS?=	${_BPS_COMPRESS}
+_PROJECT_COMPRESS.suffix.none=
+_PROJECT_COMPRESS.flag.none=
+_PROJECT_COMPRESS.suffix.gzip=	.gz
+_PROJECT_COMPRESS.flag.gzip=	-z
+_PROJECT_COMPRESS.suffix.bzip2=	.bz2
+_PROJECT_COMPRESS.flag.bzip2=	-j
+_PROJECT_COMPRESS.suffix.xz=	.xz
+_PROJECT_COMPRESS.flag.xz=	-J
 
 
 #
@@ -262,162 +148,146 @@ _PROJECT_COMPRESS.flag.xz = -J
 #  initialisation
 #
 
-.for t in ${_PROJECT_COMPRESS_TOOLS}
-.for f in ${PROJECTDISTNAME}.tar${_PROJECT_COMPRESS.suffix.${t}}
-PROJECTDISTSIGN+= ${PROJECTDISTDIR}/${f}
-PROJECTDISTEXCLUDE+= ${f}
+.for tool in ${_PROJECT_COMPRESS_TOOLS}
+.for f in ${DISTNAME}.tar${_PROJECT_COMPRESS.suffix.${tool}}
+DISTSIGN+=		${DISTDIR}/${f}
+DISTEXCLUDE+=		${f}
 .endfor
 .endfor
 
 
 #
-# Fichiers à omettre dans l'archive
+# Strategy to detect auo-excluded files
 #
 
-.for f in CVS .cvsignore .svn .gitignore .git
+.for f in CVS .cvsignore .svn .gitignore .git .product
 .if exists(${f})
-PROJECTDISTEXCLUDE+=${f}
+DISTEXCLUDE+=		${f}
 .endif
 .endfor
 
-.if target(__<bps.autoconf.mk>__)
 .if defined(CONFIGURE)&&!empty(CONFIGURE:M*.in)
 .for f in ${CONFIGURE:M*.in}
-PROJECTDISTEXCLUDE+=${PROJECTDISTNAME}/${f:.in=}
+DISTEXCLUDE+=		${DISTNAME}/${f:.in=}
 .endfor
 .endif
-.endif
 
 
 #
-# Production des archives
-#  pour de bon
+# Production rule for archives
 #
 
-.for t in ${_PROJECT_COMPRESS_TOOLS}
-${PROJECTDISTDIR}/${PROJECTDISTNAME}.tar${_PROJECT_COMPRESS.suffix.${t}}::
-	${LN} -s ${.CURDIR} ${PROJECTDISTDIR}/${PROJECTDISTNAME}
+.for tool in ${_PROJECT_COMPRESS_TOOLS}
+${DISTDIR}/${DISTNAME}.tar${_PROJECT_COMPRESS.suffix.${tool}}::
+	${LN_S} ${SRCDIR} ${DISTDIR}/${DISTNAME}
 	${TAR} -c\
-	${_PROJECT_COMPRESS.flag.${t}}\
-	-f ${.TARGET}\
-	-C ${PROJECTDISTDIR}\
-	-h\
-	${PROJECTDISTEXCLUDE:S/^/--exclude /}\
-	--exclude ${PROJECTDISTNAME}/${PROJECTDISTNAME}\
-	${PROJECTDISTNAME}
-	${RM} -f ${PROJECTDISTDIR}/${PROJECTDISTNAME}
+	  ${_PROJECT_COMPRESS.flag.${tool}}\
+	  -f ${.TARGET}\
+	  -C ${DISTDIR}\
+	  -h\
+	  ${DISTEXCLUDE:S@^@--exclude @}\
+	  --exclude ${DISTNAME}/${DISTNAME}\
+	  ${DISTNAME}
+	${RM} -f ${DISTDIR}/${DISTNAME}
 .endfor
 
 
 #
-# Production des signatures
+# Production rule for signatures
 #
 
-.for f in ${PROJECTDISTSIGN}
+.for f in ${DISTSIGN}
 ${f:=.sig}: ${f}
-	cd ${PROJECTDISTDIR};\
-	${GPG} -u '${PROJECTAUTHOR}' -b ${.ALLSRC}
+	cd ${DISTDIR} && ${GPG} -u '${OFFICER}' -b ${.ALLSRC}
 .endfor
 
 
 #
-# Préparation de la distribution
+# Targets
 #
 
 do-dist-projectdistdir:
-	${INSTALL_DIR} ${PROJECTDISTDIR}
+	${INSTALL_DIR} ${DISTDIR}
 
 
 #
-# Hospitalité
+# Hospitality
 #
 
-.for t in dist prepublish publish update
-.if target(pre-${t})
-${t}: pre-${t}
+.if !target(pre-dist)
+pre-dist:
+	${NOP}
 .endif
-${t}: do-${t}
-.if target(post-${t})
-${t}: post-${t}
+
+.if !target(post-dist)
+post-dist:
+	${NOP}
 .endif
-.endfor
 
 
 #
 # Distribution
 #
 
+.if !target(do-dist)
 do-dist: do-dist-projectdistdir
-
-.if !empty(PROJECTDIST)
-do-dist: ${PROJECTDIST}
+do-dist: do-dist-distnosign
+do-dist: do-dist-distsign
+do-dist: do-dist-sign
 .endif
 
-.if !empty(PROJECTDISTSIGN)
-do-dist: ${PROJECTDISTSIGN}
+.if !empty(DISTNOSIGN)
+do-dist-distnosign: ${DISTNOSIGN}
+.else
+do-dist-distnosign:
+	${NOP}
 .endif
 
-
-#
-# Publication
-#
-
-.if ${USE_PROJECT_GPG} == yes
-do-prepublish: ${PROJECTDISTSIGN:=.sig}
+.if !empty(DISTSIGN)
+do-dist-distsign: ${DISTSIGN}
+do-dist-sign: ${DISTSIGN:=.sig}
+.else
+do-dist-distsign do-dist-sign:
+	${NOP}
 .endif
 
-#
-# Initialisation de PROJECTBASE
-#
+do-dist-distnosign do-dist-distsign do-dist-sign: do-dist-projectdistdir
 
-.if !defined(PROJECTBASE)
-PROJECTBASE = ${.CURDIR}
-.MAKEFLAGS: PROJECTBASE="${PROJECTBASE}"
-.endif
-
+dist: pre-dist do-dist post-dist
+.ORDER: pre-dist do-dist post-dist
 
 #
-# Initialisation de PROJECTLIBRARY
+# Initialisation of PROJECTLIBRARY
 #
 
-.if !defined(PROJECTLIBRARY) && exists(${PROJECTBASE}/Library)
-PROJECTLIBRARY = ${PROJECTBASE}/Library
+.if !defined(PROJECTLIBRARY)&&exists(${SRCDIR}/Library)
+PROJECTLIBRARY=		${SRCDIR}/Library
 .endif
 
 .if defined(PROJECTLIBRARY)
-.MAKEFLAGS: PROJECTLIBRARY="${PROJECTLIBRARY}"
+.export PROJECTLIBRARY
 .endif
 
 
 #
-# Initialisation de PROJECTLIBRARYMAKE
+# Initialisation of PROJECTLIBRARYMAKE
 #
 
 .if !defined(PROJECTLIBRARYMAKE)
-.if defined(PROJECTLIBRARY) && exists(${PROJECTLIBRARY}/Make)
-PROJECTLIBRARYMAKE = ${PROJECTLIBRARY}/Make
-.elif defined(PROJECTLIBRARY) && exists(${PROJECTLIBRARY}/Mk)
-PROJECTLIBRARYMAKE = ${PROJECTLIBRARY}/Mk
-.elif defined(PROJECTBASE) && exists(${PROJECTBASE}/Mk)
-PROJECTLIBRARYMAKE = ${PROJECTBASE}/Mk
+.if defined(PROJECTLIBRARY)&&exists(${PROJECTLIBRARY}/Make)
+PROJECTLIBRARYMAKE=	${PROJECTLIBRARY}/Make
+.elif defined(PROJECTLIBRARY)&&exists(${PROJECTLIBRARY}/Mk)
+PROJECTLIBRARYMAKE=	${PROJECTLIBRARY}/Mk
+.elif exists(${SRCDIR}/Mk)
+PROJECTLIBRARYMAKE=	${SRCDIR}/Mk
 .endif
 .endif
 
 .if defined(PROJECTLIBRARYMAKE)
-.MAKEFLAGS: PROJECTLIBRARYMAKE="${PROJECTLIBRARYMAKE}"
+.export PROJECTLIBRARYMAKE
 .if empty(.MAKEFLAGS:M${PROJECTLIBRARYMAKE})
 .MAKEFLAGS: -I ${PROJECTLIBRARYMAKE}
 .endif
-.endif
-
-
-
-#
-# Reading project configuration
-#
-
-.if exists(${PROJECTLIBRARYMAKE}/project.mk)
-.include "${PROJECTLIBRARYMAKE}/project.mk"
 .endif
 
 
@@ -427,43 +297,45 @@ PROJECTLIBRARYMAKE = ${PROJECTBASE}/Mk
 
 .if !defined(PROJECTLIBRARYSHELL)
 .if defined(PROJECTLIBRARY) && exists(${PROJECTLIBRARY}/Ancillary)
-PROJECTLIBRARYSHELL = ${PROJECTLIBRARY}/Ancillary
-.elif defined(PROJECTBASE) && exists(${PROJECTBASE}/Ancillary)
-PROJECTLIBRARYSHELL = ${PROJECTBASE}/Ancillary)
+PROJECTLIBRARYSHELL=	${PROJECTLIBRARY}/Ancillary
+.elif exists(${SRCDIR}/Ancillary)
+PROJECTLIBRARYSHELL=	${SRCDIR}/Ancillary)
 .endif
 .endif
 
 
 #
-# Ouverture d'un sous-shell pour le développeur
+# Developper's subhsell
 #
-
-#PROJECTENV = MAKEFLAGS="${.MAKEFLAGS:C|-I||:C|^/|-I/|:C|^\.|-I.|}"
-# La substitution de la variable MAKEFLAGS est modifiée pour que les
-# options de type `-I' de make apparaissent sous forme compacte. Pour
-# cela, elle fait l'hypothèse que les termes commençant par un `/' ou
-# un `.' sont des chemins à traiter comme arguments pour `-I'.
 
 .if defined(PROJECTEXPORT)&&!empty(PROJECTEXPORT)
 .for v in ${PROJECTEXPORT}
-PROJECTENV+= $v="${$v}"
+PROJECTENV+=		$v="${$v}"
 .endfor
 .endif
 
 .if defined(PROJECTLIBRARYSHELL)
-PROJECTENV+= PATH="${PROJECTLIBRARYSHELL}:${PATH}"
+PROJECTENV+=		PATH="${PROJECTLIBRARYSHELL}:${PATH}"
 .endif
 
 
-# La variable SHELL est définie dans l'environnement de l'utilisateur.
-SUBSHELLDIR?= .
-subshell:
+# The SHELL variable is defined in user's environment
+SUBSHELLDIR?=		.
+subshell: .PHONY
 	${INFO} "Entering developper's subshell"
-	@cd ${SUBSHELLDIR}; ${ENVTOOL} ${PROJECTENV} ${SHELL}
+	@cd ${SUBSHELLDIR} && ${ENVTOOL} ${PROJECTENV} ${SHELL}
 	${INFO} "Exiting developper's subshell"
 
-.endif # !target(__<bps.project.mk>__)
+#
+# Delegating targets to subdirectories
+#
+
+.if defined(MODULE)
+SUBDIR+=		${MODULE:C@.*\:@@}
+.endif
 
 .include "bps.subdir.mk"
+
+.endif # !target(__<bps.project.mk>__)
 
 ### End of file `bps.project.mk'
