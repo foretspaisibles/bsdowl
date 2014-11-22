673cb46d54250c392ea49def379f91fad803c162
Michael Grünewald <michipili@gmail.com>
2014-11-22 22:19:14 +0100

    Reorganise code and comments
    Closes #92

--- config/conf.freebsd.mk
+++ config/conf.freebsd.mk
@@ -14,205 +14,224 @@
 # are also available at
 # http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt
 
-# This module defines several FILESGROUPS that can be used to install
-# configuration files on FreeBSD systems.
+# Variables:
 #
-# These files groups are:
+#  LOCALBASE [/usr/local]
+#   The prefix used by ports
 #
-#  BASE		For base system configuration files, in /etc
-#  PORT		For port configuration files, in ${LOCALBASE}/etc
-#  KERN		For kernel configuration files
-#  BOOT		For boot configuration files, in /boot
-#  CSUP		For csup configuration files, in /etc/csup
-#  FDI		For device file informations used by HAL
-#  RC		For RC files used to boot the system
-#  KDE		For KDE configuration files
-#  XDM		For XDM configuration files, in ${LOCALBASE}/lib/X11/xdm
-#  XDMPIXMAP	For XDM pixmaps
-#  XFCE		For XFCE configuration files
-
-PREFIX?=
-LOCALBASE?= /usr/local
-
-
-# BASE
 #
-#  Base system configuration files
+#  BASE [not set]
+#   Base system configuration files, belonging to /etc
 #
-#  Such as rc.conf, hosts, networks, groups, etc.
-
-FILESGROUPS+= BASE
-
-BASEDIR?= /etc
-BASEOWN?= root
-BASEGRP?= wheel
-BASEMODE?= 444
-BASEDIR.sshd_config = ${BASEDIR}/ssh
-BASEDIR.ssh_config = ${BASEDIR}/ssh
-BASEMODE.hostpad.conf = 400
-
-
-# RC
+#   Such as rc.conf, hosts, networks, groups, etc.
 #
-#  Setting up services at boot time
-
-FILESGROUPS+= RC
-
-RCDIR?= ${LOCALBASE}/etc/rc.d
-RCOWN?= ${BASEOWN}
-RCGRP?= ${BASEGRP}
-RCMODE?= 555
-
-.if defined(RC)&&!empty(RC)
-.if ${RC} == "f77"
-RC:=
-.endif
-.endif
-
-
-# PORT
 #
-#  Third party packages configurations files
+#  RCD [not set]
+#   Script setting up services at boot time
 #
-#  Such as sudoers, pkgtools.conf, apache configuration files, etc.
-
-FILESGROUPS+= PORT
-
-PORTDIR?= ${LOCALBASE}/etc
-PORTOWN?= ${BASEOWN}
-PORTGRP?= ${BASEGRP}
-PORTMODE?= 444
-
-PORTMODE.sudoers = 440
-PORTDIR.PolicyKit.conf = ${PORTDIR}/PolicyKit
-PORTDIR.fonts-local.conf = ${PORTDIR}/fonts
-PORTNAME.fonts-local.conf = local.conf
-
-# FDI
 #
-#  Device information files for the HAL system
-
-FILESGROUPS+= FDI
-
-FDIDIR?= ${LOCALBASE}/share/hal/fdi/preprobe/20thirdparty
-FDIOWN?= ${BASEOWN}
-FDIGRP?= ${BASEGRP}
-FDIMODE?= 444
-
-
-# KERN
+#  PORT [not set]
+#   Third party packages configurations files
 #
-#  Kernel configuration files
+#   Such as sudoers, pkgtools.conf, apache configuration files, etc.
 #
-#  The machine is guessed from the MACHINE variable.  Note: it would
-#  be nice to define a group for each supported machine type, so that
-#  kernels for various machines can be prepared.
-
-FILESGROUPS+= KERN
-
-KERNDIR?= /usr/src/sys/${MACHINE}/conf
-KERNOWN?= ${BASEOWN}
-KERNGRP?= ${BASEGRP}
-KERNMODE?= 444
-
-
-# BOOT
 #
-#  Loader configuration files
+#  FDI [not set]
+#   Device information files for the HAL system
 #
-#  These files usually go under /boot.
+#
+#  KERN [not set]
+#   Kernel configuration files
+#
+#   The machine is guessed from the MACHINE variable.  Note: it would
+#   be nice to define a group for each supported machine type, so that
+#   kernels for various machines can be prepared.
+#
+#
+#  BOOT [not set]
+#   Loader configuration files
+#
+#   These files usually go under /boot.
+#
+#
+#  CSUP [not set, deprecated]
+#   CVS Update files
+#
+#   Csup is a software package for updating collections of files
+#   across a network. It is used to keep FreeBSD sources and the ports
+#   collection in sync. Its configuration files goes under /etc/csup.
+#
+#   Not that this update mechanism is deprecated.
+#
+#
+#  XORG
+#   X server configuration files
+#
+#   Theses files go under /etc/X11.  Note that these files must be read
+#   somewhere to be effective, for instance in the Xsession script.
+#
+#
+#  KDE
+#   Configuration files for the K Desktop Environment
+#
+#   KDE is a desktop environment.  Among other bits of software, it
+#   provides a display manager daemon, that is a system component.  We
+#   handle KDE4.
+#
+#
+#  XDM
+#   X Display Manager
+#
+#   XDM is the display manager daemon shipped with Xorg. It is highly
+#   configurable, see comments in the relevant files.
 
-FILESGROUPS+= BOOT
 
-BOOTDIR?= /boot
-BOOTOWN?= ${BASEOWN}
-BOOTGRP?= ${BASEGRP}
-BOOTMODE?= 444
+THISMODULE=		conf.freebsd
+USE_SWITCH_CREDENTIALS=	yes
+
+LOCALBASE?=		/usr/local
 
 
-# CSUP
+#
+# BASE, for base system configuration files, in /etc
+#
+
+FILESGROUPS+=		BASE
+
+BASEDIR?=		/etc
+BASEOWN?=		root
+BASEGRP?=		wheel
+BASEMODE?=		444
+BASEDIR.sshd_config=	${BASEDIR}/ssh
+BASEDIR.ssh_config=	${BASEDIR}/ssh
+BASEMODE.hostpad.conf=	400
+
+
+#
+#  RCD, for initialisation files used to boot the system
+#
+
+FILESGROUPS+=		RCD
+
+RCDDIR?=		${LOCALBASE}/etc/rc.d
+RCDOWN?=		${BASEOWN}
+RCDGRP?=		${BASEGRP}
+RCDMODE?=		555
+
+
+#
+#  PORT, for port configuration files, in ${LOCALBASE}/etc
+#
+
+FILESGROUPS+=		PORT
+
+PORTDIR?=		${LOCALBASE}/etc
+PORTOWN?=		${BASEOWN}
+PORTGRP?=		${BASEGRP}
+PORTMODE?=		444
+
+PORTMODE.sudoers=	440
+PORTDIR.PolicyKit.conf=	${PORTDIR}/PolicyKit
+PORTDIR.fonts-local.conf=${PORTDIR}/fonts
+PORTNAME.fonts-local.conf=local.conf
+
+
+#
+# FDI, device information files for the HAL system
+#
+
+FILESGROUPS+=		FDI
+
+FDIDIR?=		${LOCALBASE}/share/hal/fdi/preprobe/20thirdparty
+FDIOWN?=		${BASEOWN}
+FDIGRP?=		${BASEGRP}
+FDIMODE?=		444
+
+
+#
+# Kernel configuration files
+#
+
+FILESGROUPS+=		KERN
+
+KERNDIR?=		/usr/src/sys/${MACHINE}/conf
+KERNOWN?=		${BASEOWN}
+KERNGRP?=		${BASEGRP}
+KERNMODE?=		444
+
+
+#
+#  Bootloader configuration files
+#
+
+FILESGROUPS+=		BOOT
+
+BOOTDIR?=		/boot
+BOOTOWN?=		${BASEOWN}
+BOOTGRP?=		${BASEGRP}
+BOOTMODE?=		444
+
+
 #
 #  CVS Update
 #
-#  Csup is a software package for updating collections of files
-#  across a network. It is used to keep FreeBSD sources and the ports
-#  collection in sync. Its configuration files goes under /etc/csup.
 
-FILESGROUPS+= CSUP
+FILESGROUPS+=		CSUP
 
-CSUPDIR?= /etc/csup
-CSUPOWN?= ${BASEOWN}
-CSUPGRP?= ${BASEGRP}
-CSUPMODE?= 444
+CSUPDIR?=		/etc/csup
+CSUPOWN?=		${BASEOWN}
+CSUPGRP?=		${BASEGRP}
+CSUPMODE?=		444
 
-CSUPDIR.refuse-supfile =/var/db/sup
-CSUPNAME.refuse-supfile = refuse
+CSUPDIR.refuse-supfile=/var/db/sup
+CSUPNAME.refuse-supfile=refuse
 
 
-# XORG
 #
 #  X server configuration files
 #
-#  Theses files go under /etc/X11.  Note that these files must be read
-#  somewhere to be effective, for instance in the Xsession script.
 
-FILESGROUPS+= XORG
+FILESGROUPS+=		XORG
 
-XORGDIR?= /etc/X11
-XORGOWN?= ${BASEOWN}
-XORGGRP?= ${BASEGRP}
-XORGMODE?= 444
+XORGDIR?=		/etc/X11
+XORGOWN?=		${BASEOWN}
+XORGGRP?=		${BASEGRP}
+XORGMODE?=		444
 
-
-# KDE
 #
 #  K Desktop Environment
 #
-#  KDE is a desktop environment.  Among other bits of software, it
-#  provides a display manager daemon, that is a system component.  We
-#  handle KDE4.
 
-FILESGROUPS+= KDE
+FILESGROUPS+=		KDE
 
-KDEDIR?= ${LOCALBASE}/kde4/share/config
-KDEOWN?= ${BASEOWN}
-KDEGRP?= ${BASEGRP}
-KDEMODE?= 444
+KDEDIR?=		${LOCALBASE}/kde4/share/config
+KDEOWN?=		${BASEOWN}
+KDEGRP?=		${BASEGRP}
+KDEMODE?=		444
 
-KDEDIR.kdmrc = ${KDEDIR}/kdm
+KDEDIR.kdmrc=		${KDEDIR}/kdm
 
 
-# XDM
 #
 #  X Display Manager
 #
-#  XDM is the display manager daemon shipped with Xorg. It is highly
-#  configurable, see comments in the relevant files.
 
-FILESGROUPS+= XDM
-XDMDIR?= ${LOCALBASE}/lib/X11/xdm
-XDMOWN?= ${BASEOWN}
-XDMGRP?= ${BASEGRP}
-XDMMODE?= 444
+FILESGROUPS+=		XDM
+XDMDIR?=		${LOCALBASE}/lib/X11/xdm
+XDMOWN?=		${BASEOWN}
+XDMGRP?=		${BASEGRP}
+XDMMODE?=		444
 
-XDMMODE.GiveConsole?= 555
-XDMMODE.TakeConsole?= 555
-XDMMODE.Xreset?= 555
-XDMMODE.Xsession?= 555
-XDMMODE.Xstartup?= 555
-XDMMODE.Xwilling?= 555
+XDMMODE.GiveConsole?=	555
+XDMMODE.TakeConsole?=	555
+XDMMODE.Xreset?=	555
+XDMMODE.Xsession?=	555
+XDMMODE.Xstartup?=	555
+XDMMODE.Xwilling?=	555
 
 .for file in ${XDM:MXsetup_*}
-XDMMODE.${file:T}?= 555
+XDMMODE.${file:T}?=	555
 .endfor
-
-FILESGROUPS+= XDMPIXMAP
-XDMPIXMAPDIR?= ${LOCALBASE}/lib/X11/xdm/pixmaps
-XDMPIXMAPOWN?= ${BASEOWN}
-XDMPIXMAPGRP?= ${BASEGRP}
-XDMPIXMAPMODE?= 444
-
-
 
 # Images goes under ${XDMDIR}/pixmaps
 .for suffix in png gif jpg jpeg xpm
@@ -221,12 +240,6 @@ XDMDIR.${file:T}?= ${XDMDIR}/pixmaps
 .endfor
 .endfor
 
-
-#
-# Epilogue
-#
-
-USE_SWITCH_CREDENTIALS = yes
 
 .include "bps.init.mk"
 .include "bps.credentials.mk"
