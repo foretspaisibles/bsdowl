7b0d27ccbac986f222b0e1f67aadc858d3165143
Michael Grünewald <michipili@gmail.com>
2014-11-29 11:22:11 +0100

    Prepare test case for Perl programs and modules

---
+++ testsuite/script/test_perl/TestPerlProgram.mk
@@ -0,0 +1,27 @@
+### TestPerlProgram.mk -- Prepare a simple Perl module
+
+# Author: Michael Grünewald
+# Date: Fri Nov 21 15:21:04 CET 2014
+
+# BSD Owl Scripts (https://github.com/michipili/bsdowl)
+# This file is part of BSD Owl Scripts
+#
+# Copyright © 2005–2014 Michael Grünewald
+#
+# This file must be used under the terms of the CeCILL-B.
+# This source file is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt
+
+
+### SYNOPSIS
+
+### DESCRIPTION
+
+.if !target(__<TestPerlProgram.mk>__)
+__<TestPerlProgram.mk>__:
+
+.endif # !target(__<TestPerlProgram.mk>__)
+
+### End of file `TestPerlProgram.mk'
