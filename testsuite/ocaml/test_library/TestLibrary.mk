c8e8f23168969ef66aef7c8423f6298c3f672bee
Michael Grünewald <michipili@gmail.com>
2014-11-29 14:52:54 +0100

    Prepare a library
    Closes #75

--- testsuite/ocaml/test_library/TestLibrary.mk
+++ testsuite/ocaml/test_library/TestLibrary.mk
@@ -1,7 +1,7 @@
-### Makefile -- Test features of module ocaml.lib
+### TestLibrary.mk -- Produce a simple library
 
 # Author: Michael Grünewald
-# Date: Sat Nov 29 08:08:34 CET 2014
+# Date: Sat Nov 29 14:47:24 CET 2014
 
 # BSD Owl Scripts (https://github.com/michipili/bsdowl)
 # This file is part of BSD Owl Scripts
@@ -14,9 +14,16 @@
 # are also available at
 # http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt
 
-TEST=			TestLibrary
-TEST+=			TestPack
+LIBRARY=		newton
 
-.include "test.mk"
+SRCS=			newton.ml
 
-### End of file `Makefile'
+.PATH:			${TESTSRCDIR}/ocaml/newton
+
+test:
+	test -f ${DESTDIR}${LIBDIR}/newton.cma
+	test -f ${DESTDIR}${LIBDIR}/newton.cmi
+
+.include "ocaml.lib.mk"
+
+### End of file `TestLibrary.mk'
