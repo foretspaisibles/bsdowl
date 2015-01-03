925dbf836ed71b9c16e472178f3dd1d714770b8f
Michael Grünewald <michipili@gmail.com>
2015-01-03 16:38:44 +0100

    Produce a custom toplevel
    Closes #89

---
+++ testsuite/ocaml/test_toplevel/TestToplevelCustom.mk
@@ -0,0 +1,36 @@
+### TestToplevelCustom.mk -- Prepare an OCaml custom toplevel
+
+# Author: Michael Grünewald
+# Date: Sat Jan  3 16:18:30 CET 2015
+
+# BSD Owl Scripts (https://github.com/michipili/bsdowl)
+# This file is part of BSD Owl Scripts
+#
+# Copyright © 2005–2015 Michael Grünewald
+#
+# This file must be used under the terms of the CeCILL-B.
+# This source file is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt
+
+TOPLEVEL=		custom_toplevel
+
+SRCS=			greeting.ml
+
+EXTERNAL+=		ocaml.findlib:compiler-libs
+
+# We provide a quick and dirty way to produce the C object, but in
+# real projects, it is reasonable to write a small library as a
+# separate project module to provide all the required stubs.
+
+CFLAGS+=		-I ${OCAMLROOTDIR}
+custom_toplevel:	greeting_stub.o
+CLEANFILES+=		greeting_stub.o
+
+test:
+	test -x ${DESTDIR}${BINDIR}/custom_toplevel
+
+.include "ocaml.toplevel.mk"
+
+### End of file `TestToplevel.mk'
