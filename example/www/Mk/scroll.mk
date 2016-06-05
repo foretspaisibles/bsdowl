694b32423bd63ed4638b9affea3b40c66091fda4
Michael Grünewald <michipili@gmail.com>
2016-06-05 15:29:46 +0200

    Port preparation of a static website to the new BSD Owl

--- example/www/Mk/scroll.mk
+++ example/www/Mk/scroll.mk
@@ -1,24 +1,39 @@
-WWWMAIN?= main.sgml
+### scroll.mk -- A simple static website
 
-SRCS+= main.sgml
-SRCS+= head-css-global.sgml
-SRCS+= more-news.sgml
-SRCS+= copyright-statement.sgml
+# BSD Owl Scripts (https://github.com/michipili/bsdowl)
+# This file is part of BSD Owl Scripts
+#
+# Copyright © 2002–2016 Michael Grünewald
+#
+# This file must be used under the terms of the CeCILL-B.
+# This source file is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt
 
-DIRS+= .
-DIRS+= ${PROJECTBASE}/style
-DIRS+= ${PROJECTBASE}/sgml
+WWWMAIN?=		main.sgml
 
-SCROLL_INCLUSION = head-css-local
-SCROLL_INCLUSION+= more-news
-SCROLL_INCLUSION+= more-ilink
-SCROLL_INCLUSION+= more-elink
-SCROLL_INCLUSION+= more-download
+SRCS=			main.sgml
+SRCS+=			head-css-global.sgml
+SRCS+=			more-news.sgml
+SRCS+=			copyright-statement.sgml
 
-.for inclusion in ${SCROLL_INCLUSION}
-.if !empty(SRCS:M${inclusion}.sgml)
-INCLUDE+= with.${inclusion}
+DIRS=			${SRCDIR}/style
+DIRS+=			${SRCDIR}/sgml
+DIRS+=			${SRCDIR}/Template
+
+SCROLL_INCLUDE=		head-css-local
+SCROLL_INCLUDE+=	more-news
+SCROLL_INCLUDE+=	more-ilink
+SCROLL_INCLUDE+=	more-elink
+SCROLL_INCLUDE+=	more-download
+
+.for include in ${SCROLL_INCLUDE}
+.if !empty(SRCS:M${include}.sgml)
+SGML_INCLUDE+=		with.${include}
 .endif
 .endfor
 
 .include "www.sgml.mk"
+
+### End of file `scroll.mk`
